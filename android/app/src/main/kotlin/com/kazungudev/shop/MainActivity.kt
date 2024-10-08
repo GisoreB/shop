3184dfcfc41649beef143421b76d53168e30f6e6
GisoreBrian <freelancerkazungudev@gmail.com>
2024-10-08 18:45:37 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/kazungudev/shop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kazungudev.shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:shop/route/route_constants.dart';
+import 'package:shop/route/router.dart' as router;
+import 'package:shop/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// Thanks for using our template. You are using the free version of the template.
+// 🔗 Full template: https://theflutterway.gumroad.com/l/fluttershop
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shop Template by The Flutter Way',
+      theme: AppTheme.lightTheme(context),
+      // Dark theme is inclided in the Full template
+      themeMode: ThemeMode.light,
+      onGenerateRoute: router.generateRoute,
+      initialRoute: onbordingScreenRoute,
+    );
+  }
+}
